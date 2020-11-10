dbe6318bf6a16bd6366cb456ebad7637001b2592
RMdS-GitHub <rmds.dev@protonmail.com>
2020-11-10 17:38:42 +0100

    The parent move state

--- src/Player/Player/Move.gd
+++ src/Player/Player/Move.gd
@@ -1,2 +1,43 @@
 extends State
+"""
+Parent state that abstracts and handles basic movement
+Move-related children states can delegate movement to it, or use its utility functions.
+"""
 
+export var max_speed_default: = Vector2(500.0, 1500.0)
+export var acceleration_default: = Vector2(10000.0, 3000.0)
+export var jump_impulse: = 900.0
+
+var acceleration: = acceleration_default
+var max_speed: = max_speed_default
+var velocity: = Vector2.ZERO
+
+
+func _physics_process(delta: float) -> void:
+	velocity = calculate_velocity(velocity, max_speed, acceleration, delta, 
+	get_move_direction())
+	velocity = owner.move_and_slide(velocity, owner.FLOOR_NORMAL)
+	Events.emit_signal("player_moved", owner)
+	
+	
+static func calculate_velocity(
+		old_velocity: Vector2,
+		max_speed: Vector2,
+		acceleration: Vector2,
+		delta: float,
+		move_direction: Vector2
+	) -> Vector2:
+	var new_velocity: = old_velocity
+	
+	new_velocity += move_direction * acceleration * delta
+	new_velocity.x = clamp(new_velocity.x, -max_speed.x, max_speed.x)
+	new_velocity.y = clamp(new_velocity.y, -max_speed.y, max_speed.y)
+	return new_velocity
+	
+
+static func get_move_direction() -> Vector2:
+	return Vector2(
+		Input.get_action_strength("move_right") - Input.get_action_strength("move_left"),
+		1.0
+	)
+	
